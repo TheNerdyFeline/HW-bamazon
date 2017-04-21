34d70a36e07d025651a87425b6999bc6159f53d9
Jackie Wellons <jackie@localhost.localdomain>
2017-04-21 13:26:34 -0700

    mySQL dump of products database

---
+++ products.sql
@@ -0,0 +1,11 @@
+id,product_name,department_name,price,stock_quantity
+1,"Charming's Choice","Pet Supplies",6.000,50
+2,Catnip,"Pet Supplies",3.500,25
+3,"Harley Pop Vynil",Collectables,10.000,10
+4,"Bulk Socks",Clothing,5.000,50
+5,"Tiger Shirt",Clothing,15.990,30
+6,"Dancing Groot",Toys,11.990,25
+7,Waterbottle,Kitchen,14.990,35
+8,"Full Set Chef Knives",Kitchen,499.990,20
+9,Flux,Toys,8.990,50
+10,"Rubber Ducky",Toys,0.990,100
